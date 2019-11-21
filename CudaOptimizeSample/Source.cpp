e5e936cede9ad073f34c05880e0befb3027eee11
Koji Ueno <koji.ueno@fixstars.com>
2019-11-21 17:42:00 +0900

    修正

--- CudaOptimizeSample/Source.cpp
+++ CudaOptimizeSample/Source.cpp
@@ -21,7 +21,7 @@
 #pragma	comment	(lib, "opencv_"	"imgcodecs"	CV_VERSION_STR	CV_SUFFIX_DBG_STR	".lib")
 #pragma	comment	(lib, "opencv_"	"imgproc"	CV_VERSION_STR	CV_SUFFIX_DBG_STR	".lib")
 
-cv::Mat GaussianFilterCPU(cv::Mat src);
+void GaussianKernelCPU(const uint8_t *src, uint8_t *dst, int width, int height, int step);
 cv::Mat GaussianFilterCPUMT(cv::Mat src);
 cv::Mat GaussianFilterGPUSimple(cv::Mat src);
 cv::Mat GaussianFilterGPUOpt(cv::Mat src, int opt);
@@ -38,28 +38,6 @@ cv::Mat BilateralFilterGPUOpt(cv::Mat src, int opt);
 cv::Mat TransposeGPU(cv::Mat src, int opt);
 cv::Mat TransposeGPUFast(cv::Mat src);
 
-
-void GaussianKernelCPU(const uint8_t *src, uint8_t *dst, int width, int height, int step)
-{
-	const float filter[5][5] = {
-		{ 0.002969017f, 0.01330621f, 0.021938231f, 0.01330621f, 0.002969017f },
-		{ 0.01330621f, 0.059634295f, 0.098320331f, 0.059634295f, 0.01330621f },
-		{ 0.021938231f, 0.098320331f, 0.162102822f, 0.098320331f, 0.021938231f },
-		{ 0.01330621f, 0.059634295f, 0.098320331f, 0.059634295f, 0.01330621f },
-		{ 0.002969017f, 0.01330621f, 0.021938231f, 0.01330621f, 0.002969017f },
-	};
-	for (int y = 0; y < height; ++y) {
-		for (int x = 0; x < width; ++x) {
-			float sum = 0;
-			for (int dy = 0; dy < 5; ++dy) {
-				for (int dx = 0; dx < 5; ++dx) {
-					sum += filter[dy][dx] * src[(x + dx) + (y + dy) * step];
-				}
-			}
-			dst[x + y * step] = (int)(sum + 0.5f);
-		}
-	}
-}
 
 void GaussianKernelCPUMT(const uint8_t *src, uint8_t *dst, int width, int height, int step)
 {
--- CudaOptimizeSample/kernel.cu
+++ CudaOptimizeSample/kernel.cu
@@ -19,6 +19,28 @@ void OnCudaError(cudaError_t err) {
 	throw "CUDA ERROR";
 }
 
+void GaussianKernelCPU(const uint8_t *src, uint8_t *dst, int width, int height, int step)
+{
+	const float filter[5][5] = {
+		{ 0.002969017f, 0.01330621f, 0.021938231f, 0.01330621f, 0.002969017f },
+		{ 0.01330621f, 0.059634295f, 0.098320331f, 0.059634295f, 0.01330621f },
+		{ 0.021938231f, 0.098320331f, 0.162102822f, 0.098320331f, 0.021938231f },
+		{ 0.01330621f, 0.059634295f, 0.098320331f, 0.059634295f, 0.01330621f },
+		{ 0.002969017f, 0.01330621f, 0.021938231f, 0.01330621f, 0.002969017f },
+	};
+	for (int y = 0; y < height; ++y) {
+		for (int x = 0; x < width; ++x) {
+			float sum = 0;
+			for (int dy = 0; dy < 5; ++dy) {
+				for (int dx = 0; dx < 5; ++dx) {
+					sum += filter[dy][dx] * src[(x + dx) + (y + dy) * step];
+				}
+			}
+			dst[x + y * step] = (int)(sum + 0.5f);
+		}
+	}
+}
+
 __global__ void GaussianKernelSimple(const uint8_t *src, uint8_t *dst, int width, int height, int step)
 {
 	const float filter[5][5] = {
